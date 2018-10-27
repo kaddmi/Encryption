9bea839a0ef76a9d4ae524fcf3f28a64ec6cb9bf
Anastasija Pischik <nastijpischik@gmail.com>
2018-10-27 21:32:07 +0300

    v1

---
+++ Encryption/Encryption.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Encryption
+    {
+        private byte[] encrypted;
+        private string publicKey;
+        private string privateKey;
+
+        public Encryption()
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                File.WriteAllText("PublicKey.xml", rsa.ToXmlString(false));
+                File.WriteAllText("PrivateKey.xml", rsa.ToXmlString(true));
+            }
+            publicKey = File.ReadAllText("PublicKey.xml");
+            privateKey = File.ReadAllText("PrivateKey.xml");
+        }
+
+        public byte[] Encrypt(byte[]data)
+        {       
+            using (var RSAEncrypt = new RSACryptoServiceProvider())
+            {
+                RSAEncrypt.FromXmlString(publicKey);
+                encrypted = RSAEncrypt.Encrypt(data, false);
+            }
+            return encrypted;
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            byte[] decrypted;
+            using (var RSADecrypt = new RSACryptoServiceProvider())
+            {
+                RSADecrypt.FromXmlString(privateKey);
+                decrypted = RSADecrypt.Decrypt(encrypted, false);
+            }
+            return decrypted;
+        }
+
+    }
+}
---
+++ Encryption/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            UnicodeEncoding Converter = new UnicodeEncoding();
+            byte[] data = Converter.GetBytes(input);
+            Encryption encryption = new Encryption();
+            string encrypted = Converter.GetString(encryption.Encrypt(data));
+            string decrypted = Converter.GetString(encryption.Decrypt(encryption.Encrypt(data)));
+            Console.WriteLine(encrypted);
+            Console.WriteLine(decrypted);
+            Console.ReadLine();
+        }
+    }
+}
